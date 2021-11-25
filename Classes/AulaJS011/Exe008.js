bb22f3aa31db574e3590de8ca4c5e8b1953f23f9
João Silva <exemplo@seuemail.com.br>
2021-11-24 21:38:18 -0400

    exe 8

---
+++ Classes/AulaJS011/Exe008.js
@@ -0,0 +1,6 @@
+var vel = 78.5
+console.log(`a sua velocidade é de ${vel}km/h`) // interpolação
+if (vel > 60){ // condicional simples
+    console.log('você ultrapassou a velocidade permitida')
+}
+console.log('Dirija com cuidado')
