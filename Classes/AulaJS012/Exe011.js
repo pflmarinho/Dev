55ab8ae39459b986acd5e748793a6571853f9c0c
João Silva <exemplo@seuemail.com.br>
2021-11-25 07:51:17 -0400

    condicional simples e composto

--- Classes/AulaJS012/Exe011.js
+++ Classes/AulaJS012/Exe011.js
@@ -1,8 +1,8 @@
 var idade = 15 
 console.log(`você tem ${idade} anos.`)       
-if(idade < 16){
+if(idade < 16){//aninhamento simples
     console.log(`nao vota`)// ultiliza o node.js
-}else if(idade < 18||idade > 65){ //aninhamento
+}else if(idade < 18||idade > 65){ //aninhamento composto
     console.log(`voto opicional`)
 }else{
     console.log('voto obrigatório')
