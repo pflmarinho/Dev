86ff77ec3f2d598ccc6f311b15025c55fa37145e
João Silva <exemplo@seuemail.com.br>
2021-11-25 07:48:44 -0400

    condicional aninhado

--- Classes/AulaJS012/Exe012.js
+++ Classes/AulaJS012/Exe012.js
@@ -1,10 +1,10 @@
 var agora = new Date
 var hora = agora.getHours()//hora do sistema do computator
-console.log(`agora sao exatamente ${hora} horas.`)
+console.log(`agora são exatamente ${hora} horas.`)
 if (hora < 12){
     console.log(`bom dia`)
 }
-else if (hora < 18){
+else if (hora < 18){//encadeamento multiplas condicoes
     console.log(`boa tarde`)
 }
 else {
