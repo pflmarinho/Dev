8aac4fb096dfe969cfbe70d35b74b205ce1c9bea
João Silva <exemplo@seuemail.com.br>
2021-11-26 10:29:16 -0400

    modelo

---
+++ Classes/AulaJS016/funcao01.js
@@ -0,0 +1,13 @@
+function parimpar(n){
+    if (n%2 == 0){
+        return "par"
+    }
+    else{
+        return "impar"
+    }
+}
+
+//let res = parimpar(4)
+//console.log(res)
+
+console.log(parimpar(7))
---
+++ Classes/AulaJS016/funcao02.js
@@ -0,0 +1,14 @@
+function soma (n1=0,n2=0){
+    res = n1+n2
+    return res
+}
+console.log(soma(2,1))
+console.log(2)
+
+const func2 = function(valor){
+    return valor * 2
+}
+const func22 = (valor) => valor * 2//arrow function
+
+console.log(func2(2))
+console.log(func22(2))
