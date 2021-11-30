14b105b0e255e9f172288c6c76d152ad516c19a6
João Silva <exemplo@seuemail.com.br>
2021-11-29 21:40:27 -0400

    aulas da udemy

---
+++ Classes/AulaJS/Fundamentos/cometário.js
@@ -0,0 +1,12 @@
+//executar fn + f8
+
+// Cometário de uma linha
+console.log("Linha 1")
+
+/*
+Comenátio 
+de 
+mútiplas
+linhas
+*/
+console.log("Linha 2")
---
+++ Classes/AulaJS/Fundamentos/var e cons.js
@@ -0,0 +1,4 @@
+var a = 3
+let b = 4 // mais moderno
+const c = 5
+console.log (a,b,c)
